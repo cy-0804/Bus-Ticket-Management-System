3718fda17d828e4e38acfd55b52a804efae18ad3
LIM ZHI YING <limzhiying03@gmail.com>
2025-07-01 23:45:33 +0800

    Add constructor in GUI

--- Flight Booking System/src/View/FlightBookingGUI.java
+++ Flight Booking System/src/View/FlightBookingGUI.java
@@ -2,4 +2,9 @@ package View;
 
 public class FlightBookingGUI {
 //i want to try yi xia see whether my git works anot - yj
+	// zy see yj's message, try me also
+	
+	public FlightBookingGUI() {
+		// TODO Auto-generated constructor stub
+	}
 }
