0460cf5bc6cb2e8cf9ced642086cc4c65014ecf6
LIM ZHI YING <limzhiying03@gmail.com>
2025-07-18 03:57:41 +0800

    test push

--- src/View/LoginGUI.java
+++ src/View/LoginGUI.java
@@ -134,7 +134,7 @@ public class LoginGUI {
 			if (status.equals("success")) {
 				JSONObject data = jsonResponse.getJSONObject("data");
 				String role = data.getString("role");
-				int userID = data.getInt("user_id");
+				int userID = data.getInt("user_id"); //userid
 				System.out.println("Login successful! Role: " + role);
 
 				if (role.equalsIgnoreCase("staff")) {
